c590444eb3f5b8064d88a11727afe128bf59e53d
Subhash0910 <89250741+Subhash0910@users.noreply.github.com>
2026-01-16 03:26:01 +0530

    Add Swagger/OpenAPI configuration for API documentation

---
+++ mini-plm-backend/src/main/java/com/sam/mini_plm_backend/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.sam.mini_plm_backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger/OpenAPI configuration for API documentation
+ * 
+ * @author Mini PLM Team
+ * @version 1.0
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("JWT token obtained from /api/auth/login")))
+                .info(new Info()
+                        .title("Mini PLM API")
+                        .description("Mini Product Lifecycle Management System API Documentation")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Mini PLM Team")
+                                .email("support@miniplm.local")
+                                .url("https://miniplm.local"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
